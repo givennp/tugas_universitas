02e082d8535ce054e68903e145153b370993998f
jrmygp <fat.snorlax17@gmail.com>
2022-03-29 15:59:10 +0700

    final CRUD merge

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-<<<<<<< HEAD
-const {
-  mahasiswaRoutes,
-  clubRoutes,
-  dosenRoutes,
-  fakultasRoutes,
-} = require("./routes");
-=======
->>>>>>> 38a288f0f6a940c0561a28b266382f9d2eef3cbb
 
 dotenv.config();
 
@@ -20,11 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-=======
 
 const { fakultasRoutes, mahasiswaRoutes, clubRoutes, matakuliahRoutes } = require("./routes")
->>>>>>> 38a288f0f6a940c0561a28b266382f9d2eef3cbb
 app.use("/mahasiswa", mahasiswaRoutes);
 app.use("/dosen", dosenRoutes);
 app.use("/fakultas", fakultasRoutes);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,4 @@
 const fakultasRoutes = require("./fakultas");
-<<<<<<< HEAD
-const mahasiswaRoutes = require("./mahasiswa");
-const clubRoutes = require("./club");
-const dosenRoutes = require("./dosen");
-
-module.exports = {
-  fakultasRoutes,
-  mahasiswaRoutes,
-  clubRoutes,
-  dosenRoutes,
-};
-=======
 const mahasiswaRoutes = require("./mahasiswa")
 const clubRoutes = require("./club")
 const matakuliahRoutes = require("./matakuliah")
@@ -22,4 +10,3 @@ module.exports = {
     matakuliahRoutes,
     dosenRoutes
 }
->>>>>>> 38a288f0f6a940c0561a28b266382f9d2eef3cbb
