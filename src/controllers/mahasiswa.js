003d5f5d02dba3a16646c74c80669771cc861d3a
rickykn <rickykurniawan350@gmail.com>
2022-03-29 15:22:49 +0700

    create edit and delete api

--- src/controllers/mahasiswa.js
+++ src/controllers/mahasiswa.js
@@ -8,7 +8,7 @@ const mahasiswaControllers = {
       const dbResult = await dbQuery(sqlQuery);
       res.status(200).json({
         message: "Find all mahasiswa",
-        result: dbQuery,
+        result: dbResult,
       });
     } catch (err) {
       console.log(err);
@@ -17,9 +17,61 @@ const mahasiswaControllers = {
       });
     }
   },
-  createNewMahasiswa: (req, res) => {},
-  editMahasiswa: (req, res) => {},
-  deleteMahasiswa: (req, res) => {},
+  createNewMahasiswa: async (req, res) => {
+    try {
+      const { mahasiswa_name } = req.body;
+
+      let sqlQuery = `INSERT INTO mahasiswa VALUES (0,?)`;
+      let replacements = [mahasiswa_name];
+      const dbResult = await dbQuery(sqlQuery, replacements);
+      res.status(201).json({
+        message: "mahasiswa added",
+        result: dbResult,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Server error",
+      });
+    }
+  },
+  editMahasiswa: async (req, res) => {
+    try {
+      const { mahasiswa_name } = req.body;
+      const mahasiswaId = req.params.id;
+
+      let sqlQuery = `UPDATE mahasiswa SET mahasiswa_name = "${mahasiswa_name}" WHERE id = ${mahasiswaId}`;
+      await dbQuery(sqlQuery);
+
+      res.status(200).json({
+        message: "edit success",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "server error",
+      });
+    }
+  },
+  deleteMahasiswa: async (req, res) => {
+    try {
+      const mahasiswaId = req.params.id;
+
+      let sqlQuery = `DELETE FROM mahasiswa WHERE id = ?`;
+      let replacements = [mahasiswaId];
+
+      await dbQuery(sqlQuery, replacements);
+
+      res.status(200).json({
+        message: "delete success",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  },
 };
 
 module.exports = mahasiswaControllers;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { mahasiswaRoutes } = require("./routes");
 
 dotenv.config();
 
@@ -11,11 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/mahasiswa");
-app.use("/dosen");
-app.use("/fakultas");
-app.use("/matakuliah");
-app.use("/club");
+app.use("/mahasiswa", mahasiswaRoutes);
+// app.use("/dosen");
+// app.use("/fakultas");
+// app.use("/matakuliah");
+// app.use("/club");
 
 app.listen(PORT, () => {
   console.log("listening in port", PORT);
