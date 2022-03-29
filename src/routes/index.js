c76ac1082c5fd57c8cee525a766bd41513216532
rickykn <rickykurniawan350@gmail.com>
2022-03-29 15:59:56 +0700

    fix conflix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,13 @@
 const fakultasRoutes = require("./fakultas");
-<<<<<<< HEAD
 const mahasiswaRoutes = require("./mahasiswa");
 const clubRoutes = require("./club");
+const matakuliahRoutes = require("./matakuliah");
 const dosenRoutes = require("./dosen");
 
 module.exports = {
   fakultasRoutes,
   mahasiswaRoutes,
   clubRoutes,
+  matakuliahRoutes,
   dosenRoutes,
 };
-=======
-const mahasiswaRoutes = require("./mahasiswa")
-const clubRoutes = require("./club")
-const matakuliahRoutes = require("./matakuliah")
-
-module.exports = {
-    fakultasRoutes,
-    mahasiswaRoutes,
-    clubRoutes,
-    matakuliahRoutes,
-    dosenRoutes
-}
->>>>>>> 38a288f0f6a940c0561a28b266382f9d2eef3cbb
