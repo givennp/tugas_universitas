0ba6a79f30dc365c0c61a44f12886542d98ab7ef
rickykn <rickykurniawan350@gmail.com>
2022-03-29 16:05:06 +0700

    merge 5

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const fakultasRoutes = require("./fakultas");
-<<<<<<< HEAD
 const mahasiswaRoutes = require("./mahasiswa");
 const clubRoutes = require("./club");
 const matakuliahRoutes = require("./matakuliah");
@@ -12,17 +11,3 @@ module.exports = {
   matakuliahRoutes,
   dosenRoutes,
 };
-=======
-const mahasiswaRoutes = require("./mahasiswa")
-const clubRoutes = require("./club")
-const matakuliahRoutes = require("./matakuliah")
-const dosenRoutes = require("./dosen")
-
-module.exports = {
-    fakultasRoutes,
-    mahasiswaRoutes,
-    clubRoutes,
-    matakuliahRoutes,
-    dosenRoutes
-}
->>>>>>> 02e082d8535ce054e68903e145153b370993998f
